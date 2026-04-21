ad05eccfab76d9d3bac0c8f29235e136dcc58f42
drini <drininazeraj14@gmail.com>
2026-04-21 17:21:38 +0200

    done

--- supabase/migrations/20260420150431_eb682be9-a8af-459b-9cf1-a774bfc8620a.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER TABLE public.cargos ADD COLUMN IF NOT EXISTS currency text NOT NULL DEFAULT 'EUR';
-ALTER TABLE public.packages ADD COLUMN IF NOT EXISTS currency text NOT NULL DEFAULT 'EUR';
-ALTER TABLE public.cargos ADD CONSTRAINT cargos_currency_check CHECK (currency IN ('EUR','USD','ALL'));
-ALTER TABLE public.packages ADD CONSTRAINT packages_currency_check CHECK (currency IN ('EUR','USD','ALL'));
